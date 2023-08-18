ccd658369ed488a129ec7f0351115fa1be2f1bb3
Sandip Biradi <sanbiradi@gmail.com>
2023-08-18 22:30:53 +0530

    route guard to myprofile route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { ProfileComponent } from './home/profile/profile.component';
 import { AuthGuard } from './auth/auth-guard.service';
-import { VisualizerComponent } from './auth/visualizer/visualizer.component';
+// import { VisualizerComponent } from './auth/visualizer/visualizer.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
   { path: 'auth/login', component: LoginComponent },
-  { path: 'visualizer', component: VisualizerComponent,},
-  { path: 'my-profile', component: ProfileComponent},
+  // { path: 'visualizer', component: VisualizerComponent,},
+  { path: 'my-profile', component: ProfileComponent ,canActivate:[AuthGuard]},
 ];
 
 @NgModule({
